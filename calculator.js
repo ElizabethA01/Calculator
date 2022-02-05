9ade6d8c2536e0f1dbdfe7ae1536f744828e7e25
Elizabeth Adejumo <a.adejumo13@gmail.com>
2022-02-05 19:46:57 +0000

    Added a bmi calculator to the web application

--- calculator.js
+++ calculator.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true})) // the code you need to write everytime you use body parser
+app.use(bodyParser.urlencoded({
+  extended: true
+})) // the code you need to write everytime you use body parser
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -16,6 +18,32 @@ app.post("/", function(req, res) {
   res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+  console.log(req.body);
+  var weight = parseFloat(req.body.weight); // passes a decimal number
+  var height = parseFloat(req.body.height);
+
+  res.send(bmiCalculator(weight, height));
+})
+
 app.listen(3000, function() {
   console.log("server is ready");
 });
+
+function bmiCalculator(weight, height) {
+  var bmi = Math.round(weight / (height * height));
+  if (bmi < 18.5) {
+    var interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  }
+  if (bmi >= 18.5 && bmi <= 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you have a normal weight."
+  }
+  if (bmi > 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you are overweight."
+  }
+  return interpretation;
+}
